9fb9226b2d9bd7f8eef8262700d2215253313bcf
David Adams <dadams@instructure.com>
2018-05-04 10:06:30 -0500

    Add VPCs, subnets, and security groups to ec2.go

--- ec2.go
+++ ec2.go
@@ -4,7 +4,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
-// Returns _all_ of the EC2 instances in the given region.
+// Returns all of the EC2 instances in the given region.
 func (a *Account) AllInstances(region string) ([]*ec2.Instance, error) {
 	resp, err := a.EC2(region).DescribeInstances(&ec2.DescribeInstancesInput{})
 	if err != nil {
@@ -19,4 +19,34 @@ func (a *Account) AllInstances(region string) ([]*ec2.Instance, error) {
 	}
 
 	return rv, nil
+}
+
+// Returns all VPCs in the given region.
+func (a *Account) AllVpcs(region string) ([]*ec2.Vpc, error) {
+	resp, err := a.EC2(region).DescribeVpcs(&ec2.DescribeVpcsInput{})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Vpcs, nil
+}
+
+// Returns all subnets in the given region.
+func (a *Account) AllSubnets(region string) ([]*ec2.Subnet, error) {
+	resp, err := a.EC2(region).DescribeSubnets(&ec2.DescribeSubnetsInput{})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Subnets, nil
+}
+
+// Returns all security groups in the given region.
+func (a *Account) AllSecurityGroups(region string) ([]*ec2.SecurityGroup, error) {
+	resp, err := a.EC2(region).DescribeSecurityGroups(&ec2.DescribeSecurityGroupsInput{})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.SecurityGroups, nil
 }
